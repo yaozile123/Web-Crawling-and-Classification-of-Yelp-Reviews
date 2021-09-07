51eed3ddd93a55f21320dbee2e292b88a9236f5d
zile yao <yaozl2000@163.com>
2021-09-07 14:40:10 -0500

    import all the package I needed

--- Model fitting.py
+++ Model fitting.py
@@ -2,7 +2,13 @@
 # coding: utf-8
 
 # In[ ]:
-
-
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.preprocessing import StandardScaler
+from sklearn import tree
+import lightgbm as lgb
 
 
