c91584eed732635338a43c2e43da3395579eb6b9
zile yao <yaozl2000@163.com>
2021-09-07 13:00:24 -0500

    add spaces and \n

--- Collecting_data.py
+++ Collecting_data.py
@@ -30,7 +30,12 @@ def get_review_content(baseurl,start,end):
     
 '''
 Here is the sample of review from yelp
-"review: Great!  I love this place.  I found it by chance last fall while in NYC on a street photography trip.  Very friendly staff, tasty food at reasonable prices, good beers selection too.   I went back 3 times that week and would have gone again had my visit not come to an end.  There_s a beer garden-like court yard area and when the weather is good, like when I was there, they have all the doors open so a nice breeze was passing through the main eating area; sort of felt like a pleasant little greenhouse.  I_m no NYC expert but this place felt like a little oasis.",
+"review: Great!  I love this place.  I found it by chance last fall while in NYC on a street photography 
+trip.  Very friendly staff, tasty food at reasonable prices, good beers selection too.   I went back 3 times 
+that week and would have gone again had my visit not come to an end.  There_s a beer garden-like court yard 
+area and when the weather is good, like when I was there, they have all the doors open so a nice breeze was 
+passing through the main eating area; sort of felt like a pleasant little greenhouse.  I_m no NYC expert but 
+this place felt like a little oasis.",
       "score: 4",
       "total number of scores:{4: 104, 3: 633, 2: 286, 5: 22, 1: 130}"
 
