39e3faeada2788d2501dc89e5616827b156fac4a
zile yao <yaozl2000@163.com>
2021-09-07 14:35:37 -0500

    functionlize

--- Data_cleaning.py
+++ Data_cleaning.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+def data_cleaning(scores):
+    y=[] # if score less or equal to 3, we will assign it to 0 which mean bad review,otherwise we will assign it to 1 which means good review
+    for i in range(len(scores)):
+        if(scores[i]<=3):
+            y.append(0)
+
+        else:
+            y.append(1)
+    return y
 
 
 
