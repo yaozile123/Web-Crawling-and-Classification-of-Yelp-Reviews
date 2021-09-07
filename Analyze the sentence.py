5c571fa111bbe33e575d0df5defd93978f7af54b
zile yao <yaozl2000@163.com>
2021-09-07 14:55:26 -0500

    functionlize

--- Analyze the sentence.py
+++ Analyze the sentence.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+
+vectorizer = CountVectorizer(max_df=0.8,min_df=0.15) #choose the vectorizer you want
+#vectorizer = TfidfVectorizer(max_df=0.8,min_df=0.15) #de-annotation if you wanna use tf-idf
+def analyze_sentence(vectorizer, reviews):
+    vectorizer.fit(reviews)
+    X = vectorizer.transform(reviews)
+    rerurn X
 
 
 
