d4fd35c2c362dddb8f4ca69630be5a81273f1ddf
zile yao <yaozl2000@163.com>
2021-09-08 15:51:09 -0500

    11

--- Model fitting.py
+++ Model fitting.py
@@ -12,3 +12,15 @@ from sklearn import tree
 import lightgbm as lgb
 
 
+model = LogisticRegression()#logistic regression
+model = svm.SVC()#svm
+model = tree.DecisionTreeClassifier()#decision_tree
+model = lgb.LGBMClassifier() #lightGBM
+
+def model_fitting(model, X, y, test_size):
+    X_train,X_test,y_train,y_test = train_test_split(X.todense(),y,test_size=test_size,random_state=42) #split the data to train set and test set
+    model.fit(X_train,y_train) #fit the model that you choose
+    y_pred = model.predict(X_test) #predict
+    print('training done')
+    return y_pred
+    
