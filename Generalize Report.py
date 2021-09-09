32c958e8219979b4c108b63bfe89670b62754057
zile yao <yaozl2000@163.com>
2021-09-09 13:16:03 -0500

    1

--- Generalize Report.py
+++ Generalize Report.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
+from sklearn.metrics import classification_report
+def generalize_report(y_test,y_pred):
+    r = classification_report(y_test,y_pred)
+    print(r)
 
 
 
